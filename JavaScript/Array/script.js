56ef4f2924e4bb88ec6ca75c20331317ad598c1d
Pandabug <71569878+Pandabug@users.noreply.github.com>
2021-03-18 00:59:01 +0100

    [JS]

--- JavaScript/Array/script.js
+++ JavaScript/Array/script.js
@@ -11,18 +11,18 @@ function array(){
     for (var i=0; i<number; i++)
         a[i] = Math.floor(50 * Math.random()) + 1;
         
-    document.getElementById("out").innerHTML = Math.max(...a);
     
     var outNumbers = "";
     print(a, number);
 }
 
 function print(a, number){
-    document.write("<table border=1><tr>")
+    // document.write("<table border=1><tr>")
 
-    for (var i=0; i<number; i++)
-        document.write("<td>" + a[i] + "</td>")
+    // for (var i=0; i<number; i++)
+        // document.write("<td>" + a[i] + "</td>")
 
-    document.write("</tr></table>")
-
+    // document.write("</tr></table>")
+    document.getElementById('out').innerHTML = "<table><tr><td>" + a.toString() + "</td></tr></table>";
+    document.getElementById("outMax").innerHTML = Math.max(...a);
 }
