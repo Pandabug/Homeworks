b55a8025cd1ff6ad5efe1a7fb6332976b686eb1d
Bednarchuk Ivan-Vitaliy <xyjiigah.ib@gmail.com>
2022-05-16 11:50:13 +0200

    [Java] 9 - Chat

---
+++ Java/9 - Nazione/Capitale.java
@@ -0,0 +1,20 @@
+public class Capitale {
+    String nomeC;
+    Nazione nazione;
+
+    public Capitale(String nome) {
+        this.nomeC = nome;
+    }
+
+    public String getNomeC() { 
+        return nomeC; 
+    }
+
+    public Nazione getNazione() { 
+        return nazione; 
+    }
+
+    public void setNazione(Nazione nazione) {
+        this.nazione = nazione;
+    }
+}
